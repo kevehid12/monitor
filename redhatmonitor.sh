7582e693335c06dcd0539f10270ccdae158fc35b
kevehid12 <40859983+kevehid12@users.noreply.github.com>
2019-09-14 20:28:48 +0200

    Update redhatmonitor.sh

--- redhatmonitor.sh
+++ redhatmonitor.sh
@@ -9,7 +9,7 @@ content= $logfile
 
 if [ -s /tmp/12.txt ] 
 then
-	curl -X POST --data-urlencode 'payload={ "username": "webhookbot", "text": "Failed login has been detected, please check the server for more informaiton", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+	curl -X POST --data-urlencode 'payload={ "username": "webhookbot", "text": "Failed login has been detected, please check the server for more informaiton", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/TJP2FLEDR/BK2AGTL5D/blabla
 else
 	echo "not malicious"
 fi
